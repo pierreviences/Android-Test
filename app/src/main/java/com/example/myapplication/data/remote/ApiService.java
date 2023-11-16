1ea0ed7890ea6b61a8dccd6e8435b99d043e54fa
pierreviences <imaniyazid@gmail.com>
2023-11-16 15:37:40 +0700

    feat: add apiservice getall data

---
+++ app/src/main/java/com/example/myapplication/data/remote/ApiService.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.data.remote;
+
+import com.example.myapplication.data.model.job.JobResponse;
+import com.example.myapplication.data.model.job.JobResponseItem;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+    @GET("positions.json")
+    Call<List<JobResponseItem>> getJob();
+}
