15634fb940bcfef93638def1fd32dce56b574f25
pierreviences <imaniyazid@gmail.com>
2023-11-16 15:39:03 +0700

    feat: add viewmodel

---
+++ app/src/main/java/com/example/myapplication/ui/MainViewModel.java
@@ -0,0 +1,23 @@
+package com.example.myapplication.ui;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.myapplication.data.model.job.JobResponse;
+import com.example.myapplication.data.model.job.JobResponseItem;
+import com.example.myapplication.data.repository.JobRepository;
+import com.example.myapplication.utils.Result;
+
+import java.util.List;
+
+public class MainViewModel extends ViewModel {
+    private final JobRepository jobRepository;
+    public MainViewModel(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+    public LiveData<Result<List<JobResponseItem>>> getJob() {
+        return jobRepository.getJob();
+    }
+
+}
