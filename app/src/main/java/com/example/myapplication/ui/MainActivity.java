e4cfc2ddcc930652588539d2c1d7acc8b97e33d1
pierreviences <imaniyazid@gmail.com>
2023-11-16 15:39:56 +0700

    feat: connect the JobAdapter with the MainActivity

---
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.example.myapplication.data.model.job.JobResponseItem;
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.example.myapplication.ui.adapter.JobAdapter;
+import com.example.myapplication.ui.factory.ViewModelFactory;
+import com.example.myapplication.utils.Result;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    private MainViewModel mainViewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        ViewModelFactory factory = ViewModelFactory.getInstance(this);
+        mainViewModel = new ViewModelProvider(this, factory).get(MainViewModel.class);
+
+        JobAdapter jobAdapter = new JobAdapter();
+
+        mainViewModel.getJob().observe(this, result -> {
+            if (result != null) {
+                if (result instanceof Result.Loading){
+                    binding.progressBar.setVisibility(View.VISIBLE);
+                } else if (result instanceof Result.Success){
+                    binding.progressBar.setVisibility(View.GONE);
+                    List<JobResponseItem> jobData = ((Result.Success<List<JobResponseItem>>) result).getData();
+                    jobAdapter.submitList(jobData);
+                } else if (result instanceof Result.Error){
+                    binding.progressBar.setVisibility(View.GONE);
+                    Toast.makeText(this, "Terjadi kesalahan " + ((Result.Error<List<JobResponseItem>>) result).getError(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding.rvJob.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvJob.setHasFixedSize(true);
+        binding.rvJob.setAdapter(jobAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/adapter/JobAdapter.java
@@ -0,0 +1,78 @@
+package com.example.myapplication.ui.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.myapplication.R;
+import com.example.myapplication.data.model.job.JobResponseItem;
+
+import java.util.Objects;
+import androidx.recyclerview.widget.ListAdapter;
+
+
+public class JobAdapter extends ListAdapter<JobResponseItem, JobAdapter.MyViewHolder> {
+    public JobAdapter() {
+        super(DIFF_CALLBACK);
+    }
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_job, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        JobResponseItem jobItem = getItem(position);
+        if (jobItem != null) {
+            holder.bind(jobItem);
+        }
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvTitle, tvCompany, tvLocation;
+        private  ImageView imgTitle;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvCompany = itemView.findViewById(R.id.tvCompany);
+            tvLocation = itemView.findViewById(R.id.tvLocation);
+            imgTitle = itemView.findViewById(R.id.imgTitle);
+        }
+
+        public void bind(JobResponseItem jobItem) {
+            tvTitle.setText(jobItem.getTitle());
+            tvCompany.setText(jobItem.getCompany());
+            tvLocation.setText(jobItem.getLocation());
+            Glide.with(itemView)
+                    .load(jobItem.getCompanyLogo())
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .into(imgTitle);
+        }
+    }
+
+    private static final DiffUtil.ItemCallback<JobResponseItem> DIFF_CALLBACK =
+            new DiffUtil.ItemCallback<JobResponseItem>() {
+                @Override
+                public boolean areItemsTheSame(@NonNull JobResponseItem oldItem, @NonNull JobResponseItem newItem) {
+                    return Objects.equals(oldItem.getId(), newItem.getId());
+                }
+
+                @Override
+                public boolean areContentsTheSame(@NonNull JobResponseItem oldItem, @NonNull JobResponseItem newItem) {
+                    return Objects.equals(oldItem, newItem);
+                }
+            };
+}
